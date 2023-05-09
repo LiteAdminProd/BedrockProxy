2d394f96ce7c4ad2bf43baf4e52541f5147e31b3
Vinky Nightly <ferfet101@gmail.com>
2023-05-10 00:08:54 +0300

    Initialize project!

---
+++ main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net"
+
+	"github.com/sandertv/gophertunnel/minecraft"
+	"github.com/sandertv/gophertunnel/minecraft/protocol/packet"
+)
+
+func main() {
+
+	const addr = "localhost:228"
+
+	status, err := minecraft.NewForeignStatusProvider(addr)
+	if err != nil {
+		log.Panic(err)
+	}
+	listen := minecraft.ListenConfig{
+		StatusProvider:         status,
+		AuthenticationDisabled: false,
+		MaximumPlayers:         10,
+		PacketFunc:             handle,
+	}
+	listener, err := listen.Listen("raknet", addr)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	defer listener.Close()
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			panic(err)
+		}
+		fmt.Printf("conn: %v\n", conn.RemoteAddr())
+	}
+
+}
+
+func handle(header packet.Header, payload []byte, src net.Addr, dst net.Addr) {
+	log.Print(header.PacketID)
+}
